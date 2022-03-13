c6832b4d6d9629402abe3ebd85a5df41a065feb4
yada17 <yaredmesele17@gmail.com>
2022-03-13 05:00:09 -0700

    second trial

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 int main (void)
 {
-	puts("programming is like building a multilingual puzzle\n");
+	puts("\"programming is like building a multilingual puzzle\n");
 	return 0;
 
 }
