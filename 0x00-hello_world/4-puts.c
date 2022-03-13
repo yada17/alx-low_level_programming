b675ced8c4e5f6644a770c316c687563530282bf
yada17 <yaredmesele17@gmail.com>
2022-03-13 08:00:12 -0700

    bettty

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 
+/**
+ *main - Entry point
+ *
+ *Return: Always 0 (success)
+ */
 int main(void)
-/*this is mandatory*/
 {
 	puts("\"programming is like building a multilingual puzzle");
 	return (0);
