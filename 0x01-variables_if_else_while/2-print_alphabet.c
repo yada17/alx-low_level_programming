20cedb5531c35c4250814435640807395e7b505b
yada17 <yaredmesele17@gmail.com>
2022-03-14 09:22:21 -0700

    do it for lid

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+*main - Entry level
+*
+*Return: Always 0 (Success)
+*/
+int main(void)
+{
+	char c;
+	for ( c = 'a'; c <= 'z'; ++c)
+	putchar(c);
+	return (0);
+}
