81161bbae6a8064e8d0facfe893662302a8030e3
yada17 <yaredmesele17@gmail.com>
2022-03-14 10:46:50 -0700

    many mistakes but many lessons

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -13,9 +13,9 @@ int main(void)
 	char c;
 
 	for (c = 'a'; c <= 'z'; ++c)
-		putchar(c);
+	putchar(c);
 	for (c = 'A'; c <= 'Z'; ++c)
-		putchar(c);
-		putchar('\n');
+	putchar(c);
+	putchar('\n');
 	return (0);
 }
