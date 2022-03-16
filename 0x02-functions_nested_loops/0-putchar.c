64f76973429b5cbf309f70acb71fa48d0079c3bf
yada17 <yaredmesele17@gmail.com>
2022-03-16 05:39:48 -0700

    the first code

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include"main.h"
+/**
+ * main - entry block
+ * *@void: no argument
+ * Return: 0
+ * **/
+int main(void)
+{
+	char yada[] = "_putchar";
+
+	int c:
+	
+	for (c=0; c < 8; c++)
+	{
+		_putchar(yada[c]);
+	}
+	_putchar('\n');
+	return (0);
---
+++ 0x02-functions_nested_loops/_putchar.c
@@ -0,0 +1,14 @@
+
+
+#include "unistd.h"
+/**
+*_putchar - writes the charcter c to stdout
+*@c: Charachtet to print
+*
+*Return: on success 1'
+*on error, -1 is returned. and errno is set appropriately
+*/
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
