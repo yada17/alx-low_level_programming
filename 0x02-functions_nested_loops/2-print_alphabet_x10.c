04c811150a7363155b0f623632ed77deef72cbfe
yada17 <yaredmesele17@gmail.com>
2022-03-18 01:18:34 +0300

    pikjwr

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -10,10 +10,10 @@ void print_alphabet_x10(void)
 	char c;
 
 	for (i = 0; i < 10; i++)
-	{	
-		for (c = 'a'; c <= 'z'; c++)
-			_putchar(c);
-		_putchar('\n');
+	{
+	for (c = 'a'; c <= 'z'; c++)
+		_putchar(c);
+	_putchar('\n');
 	}
 
 }
