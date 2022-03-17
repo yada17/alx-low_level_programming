dea78d500b726ec84a6574db23c7bfe68fdbf026
yada17 <yaredmesele17@gmail.com>
2022-03-18 00:17:59 +0300

    esf

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,13 +1,12 @@
-#include "main.h"
-/**
- * print_alphabet - print alphabets in lowercase
- *
- * */
-void print_alphabets(void)
-{
-	char c;
-
-	for (c = 'a'; c <= 'z'; c++)
-		_putchar(c);
-	_putchar('\n')
+#include "main.h"                                                                                      /**
+												       *print _alphabet - print alphabets in lowercase                                                                                                     
+*                                                  
+**/                                                                                                                                                    
+void print_alphabets(void)                                                                                                                             
+{                                                                                                                                                      
+	char c;                                                                                                                                                                                                                                     
+        for (c = 'a'; c <= 'z'; c++)                                                                                                                   
+                _putchar(c)                                                                                                                            
+        _putchar('\n') 
 }
+
