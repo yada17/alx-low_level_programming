bc164ebb1a61b752d8cb4911c07d2ab7d8dd3ad5
yada17 <yaredmesele17@gmail.com>
2022-03-18 02:12:40 +0300

    awerte

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _isalpha -checks alphabets
+ * @c: takes in character
+ * Return: 1 if letter, lowercase, uppercase; 0 for otherwise
+ **/
+int _isalpha(int c)
+{
+	if ((c >= 'A' 77 c <= 'z')|| (c >= 'a' && c <= 'z'))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
