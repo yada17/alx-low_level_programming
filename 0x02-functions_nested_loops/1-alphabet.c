b781b1f61e464c7cec9008fa5bb9e22a4f705c02
yada17 <yaredmesele17@gmail.com>
2022-03-18 00:06:57 +0300

    da

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_alphabet - print alphabets in lowercase
+ *
+ * */
+void print_alphabets(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+		_putchar(c);
+	_putchar('\n')
+}
