49e7a1c32ac5a0c40885e0a8a383becb61e3fcdd
yada17 <yaredmesele17@gmail.com>
2022-03-18 03:06:39 +0300

    weqqer

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- *  prints_last_digit - function that prints the last digit
+ *  print_last_digit - function that prints the last digit
  *  @n: takes in an integer
  *  Return: returns output
  **/
@@ -9,7 +9,7 @@ int print_last_digit(int n)
 	int ln = n % 10;
 
 	if (n < 0)
-		ln = ln *-1;
+		ln = ln * -1;
 	_putchar(ln + '0');
 	return (ln);
 }
--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -6,6 +6,7 @@
 void jack_bauer(void)
 {
 	int i, j;
+
 	for (i = 0; j < 24; j++)
 	{
 		for (j = 0; i < 24; i++)
