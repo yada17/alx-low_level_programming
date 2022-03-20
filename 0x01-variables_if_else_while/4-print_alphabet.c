e650ae180547ea1120b94d5f44baa1222daef1c5
yada17 <yaredmesele17@gmail.com>
2022-03-20 03:13:14 -0700

    this is done

---
+++ 0x01-variables_if_else_while/4-print_alphabet.c
@@ -0,0 +1,23 @@
+
+
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+*main - Entry level
+*
+*Return: Always 0 (Success)
+*/
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'd'; ++c)
+	putchar(c);
+	for (c = 'f'; c <= 'p'; ++c)
+	putchar(c);
+	for (c = 'r'; c <= 'z'; ++c)
+	putchar(c);
+	putchar('\n');
+	return (0);
+}
