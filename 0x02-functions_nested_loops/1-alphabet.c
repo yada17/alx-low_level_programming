3833c25b819af350838c0ef68116544c5227f940
yada17 <yaredmesele17@gmail.com>
2022-03-30 01:47:49 +0300

    asg

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
 * main - prints alphabets
-*
+* void - return 0
 * print_alphabet - print alphabets in lowercase
 **/
 void print_alphabet(void)
@@ -10,7 +10,6 @@ void print_alphabet(void)
 	char c;
 
 	for (c = 'a'; c <= 'z'; c++);
-
 	_putchar(c);
 	_putchar('\n');
 }
